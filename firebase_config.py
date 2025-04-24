42d9a2c6e8bfadcf7d23c887b70492fbea87d048
tranthanhthangbmt <thangtt@donga.edu.vn>
2025-04-24 16:45:01 +0700

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,9 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import json
+import os
 
-# Khởi tạo Firebase App (chỉ một lần)
 def init_firestore():
     if not firebase_admin._apps:
-        cred = credentials.Certificate("serviceAccountKey.json")  # file bạn tải từ Firebase Console
+        service_account_info = json.loads(os.environ["FIREBASE_CREDENTIALS"])
+        cred = credentials.Certificate(service_account_info)
         firebase_admin.initialize_app(cred)
     return firestore.client()
