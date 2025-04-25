bb2bbf8ec121e3986fdc9c971ba00aeed3d4a7be
tranthanhthangbmt <thangtt@donga.edu.vn>
2025-04-25 11:26:13 +0700

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -6,7 +6,8 @@ import streamlit as st  # ✅ Để dùng st.secrets
 def init_firestore():
     if not firebase_admin._apps:
         # ✅ Đúng cách trên Streamlit Cloud
-        service_account_info = json.loads(st.secrets["FIREBASE_CREDENTIALS"])
+        #service_account_info = json.loads(st.secrets["FIREBASE_CREDENTIALS"])
+        service_account_info = st.secrets["FIREBASE_CREDENTIALS"]
         cred = credentials.Certificate(service_account_info)
         firebase_admin.initialize_app(cred)
     return firestore.client()
