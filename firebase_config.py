859ba6c2083c14e1a180799f8974e6b639ca643b
tranthanhthangbmt <thangtt@donga.edu.vn>
2025-04-25 11:30:12 +0700

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,14 +1,19 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import streamlit as st
 import json
-import streamlit as st  # ✅ Để dùng st.secrets
+import tempfile
 
 def init_firestore():
     if not firebase_admin._apps:
-        # ✅ Đúng cách trên Streamlit Cloud
-        #service_account_info = json.loads(st.secrets["FIREBASE_CREDENTIALS"])
         service_account_info = st.secrets["FIREBASE_CREDENTIALS"]
-        #cred = credentials.Certificate(service_account_info)
-        cred = credentials.Certificate.from_json(service_account_info) 
+
+        # Ghi JSON credentials ra file tạm
+        with tempfile.NamedTemporaryFile(mode="w", suffix=".json", delete=False) as temp_file:
+            json.dump(service_account_info, temp_file)
+            temp_file.flush()  # đảm bảo nội dung được ghi đầy đủ
+            cred = credentials.Certificate(temp_file.name)
+
         firebase_admin.initialize_app(cred)
+
     return firestore.client()
